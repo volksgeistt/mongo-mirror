1dc3eca11780d2b310c087d284b3cbb959765f95
Ujjawal Singh <131983323+volksgeistt@users.noreply.github.com>
2024-12-31 10:11:09 +0530

    Update mongo.py

--- mongo.py
+++ mongo.py
@@ -1,7 +1,6 @@
 from pymongo import MongoClient
 import logging
 
-
 def cloneDatabase(sourceURL: str, otherURL: str):
     logging.basicConfig(level=logging.INFO)
     logger = logging.getLogger(__name__)
@@ -12,35 +11,35 @@ def cloneDatabase(sourceURL: str, otherURL: str):
         
         sourceDB = sourceUser.get_default_database()
         if not sourceDB:
-            raise ValueError("No database specified in source connection string")
+            raise ValueError("The Provided Source Connection Has No Specified DATABASE.")
         
         dbName = sourceDB.name
         otherDB = otherUser[dbName]
-        logger.info(f"Connected to databases. Cloning database: {dbName}")
+        logger.info(f"Connected To DATABASE — Cloning — {dbName}")
         
         allCollections = sourceDB.list_collection_names()
         for collectionName in allCollections:
-            logger.info(f"Processing collection: {collectionName}")
+            logger.info(f"Processing collection — {collectionName}")
             sourceCollection = sourceDB[collectionName]
             otherCollection = otherDB[collectionName]
             
             documents = list(sourceCollection.find({}))
             if documents:
-                logger.info(f"Found {len(documents)} documents in {collectionName}")
+                logger.info(f"Found `{len(documents)}` Docs In — {collectionName}")
                 otherCollection.insert_many(documents)
-                logger.info(f"Transferred {collectionName}")
+                logger.info(f"Transferred — {collectionName}")
             else:
-                logger.info(f"Collection {collectionName} is empty")
+                logger.info(f"Collection `{collectionName}` is empty")
                 
-        logger.info("Database clone  successfully!")
+        logger.info("Database Cloned — Successfully")
         
     except Exception as e:
-        logger.error(f"Error during cloning: {str(e)}")
+        logger.error(f"Error While Cloning — {str(e)}")
         raise
     finally:
         sourceUser.close()
         otherUser.close()
-        logger.info("Database connections closed")
+        logger.info("Database Connection — Closed")
 
 
 if __name__ == "__main__":
